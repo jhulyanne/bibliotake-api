4728cdba90f05c558d7c44de72341fa5bca81955
jhulyanne <jhulyannedeoliveira@gmail.com>
2025-06-27 02:02:39 -0300

    :package: chore: update server port configuration to use environment variable

--- src/index.js
+++ src/index.js
@@ -4,24 +4,10 @@ import loanRoutes from "./routes/loan.routes.js"
 import userRoutes from "./routes/user.routes.js"
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// ↓ Pode ser implementado no futuro quando houverem outros tipos de rota
-// app.use(json);
-
-// TESTADAS - estão OK e funcionando
-// books/ - retorna todos os livros do bd
-// books/<id> - retorna livro de id espeífico
 app.use("/books", booksRoutes);
-
-// TESTADAS - estão OK e funcionando
-// loans/ - retorna todos os empréstimos do bd
-// loans/<nome> - retorna todos os empréstimos feito por usuário (parâmetro: nome exato - depende de maiúsculas e espaços)
 app.use("/loans", loanRoutes);
-
-// TESTADAS - estão OK e funcionando
-// users/ - retorna todos os usuários do bd
-// users/<elemento> - retorna todos os empréstimos feito por usuário (parâmetro: elemento do endereço, seja cidade, estado - independe de maiúsculas, depende de espaços)
 app.use("/users", userRoutes);
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta: http://localhost:${PORT}`))
