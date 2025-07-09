7920ba1f992bf80c4cfd1bb13c669274719aa1a4
jhulyanne <jhulyannedeoliveira@gmail.com>
2025-07-09 09:45:19 -0300

    :package: add CORS middleware and update dependencies in package.json and package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors"; 
 import booksRoutes from "./routes/book.routes.js"
 import loanRoutes from "./routes/loan.routes.js"
 import userRoutes from "./routes/user.routes.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: 'https://bibliotake.vercel.app', // URL exata do frontend, que é só quem vai poder acessar a API por enquanto
+  methods: ['GET'] // Só permite consultas (único método por enquanto) * NO FUTURO: mudar isso para ['GET', 'POST', 'PUT', 'DELETE'] se for necessário
+}));
 
 app.use("/books", booksRoutes);
 app.use("/loans", loanRoutes);
